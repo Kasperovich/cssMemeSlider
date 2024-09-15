6a915924aee6550f30e627607a1c8c0eb48fc2c8
kasperovich <kasperovich.v.d@gmail.com>
2024-09-15 18:42:03 +0300

    feat: add slider animation 15.09.2024 18:41

--- cssMemeSlider/index.js
+++ cssMemeSlider/index.js
@@ -1,15 +1,39 @@
+function ChangeSlide(memeNumber){
+    var slider = document.getElementsByClassName('slider-all')[0];
+
+    slider.style.transform = 'translate(-'+slider.parentNode.offsetWidth * memeNumber+'px)';
+    activeButton(memeNumber);
+}
+
 function imageResize(){
     //Получаем блок слайдера
-    var slider = document.getElementsByClassName('slider-all')[0];
-    var sliderItems = document.querySelectorAll('.slider-all img');
+    var sliderAll = document.getElementsByClassName('slider-all')[0];
+    var sliderItems = document.getElementsByClassName('slider-all-img');
 
     //Изменение ширины изображения согласно ширине слайдера 
-    sliderItems.forEach(item=>{
-        item.style.width = slider.style.width;
+    Array.from(sliderItems).forEach(item=>{
+        item.style.width = sliderAll.offsetWidth + 'px';
+    });
+}
+
+function activeButton(memeNumber){
+    var buttonCircle = document.getElementsByClassName('circle');
+    var index = 0;
+
+    Array.from(buttonCircle).forEach(item=>{
+
+        item.classList.remove('footer-navigation-button-circle1');
+        item.classList.remove('footer-navigation-button-circle');
+
+        if(index == memeNumber)
+            item.classList.add('footer-navigation-button-circle1');
+        else 
+            item.classList.add('footer-navigation-button-circle');
+        index++;
     });
 }
 
 window.addEventListener('resize', function() {
+    ChangeSlide(0);
     imageResize();
-    console.log('resize');
 });
